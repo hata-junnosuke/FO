895f97baec40d0c012c0c5cafeb9b9cf848a2a18
hata-junnosuke <jh19911209@icloud.com>
2022-03-13 21:17:06 +0900

    バリデーション漏れを修正

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   # 今回の課題には関係ないがつけるのが一般的と考え文字数制限を入れる
-  validates :code, presence: true, length: { maximum: 10 }
+  validates :code, presence: true, uniqueness: true, length: { maximum: 10 }
   validates :name, presence: true, length: { maximum: 50 }
   validates :price,
             presence: true,
